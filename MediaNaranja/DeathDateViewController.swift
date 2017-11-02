3887c2cb05e9296cf8ade91418e0e1e290c649b2
Pablo Yáñez <pablosyz@Pablos-Mac.local>
2017-11-02 12:29:46 +0100

    1st version

---
+++ MediaNaranja/DeathDateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeathDateViewController.swift
+//  MediaNaranja
+//
+//  Created by Pablo Sanchez Yáñez on 27/10/17.
+//  Copyright © 2017 Pablo Sanchez Yáñez. All rights reserved.
+//
+
+import UIKit
+
+class DeathDateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
